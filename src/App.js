e0932bac2440648310604d99ec25718fcb514ea8
ALEXANDRE BERGERON <balex2001@outlook.com>
2022-05-17 13:09:12 -0400

    4. Styled Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//Styles
+import GlobalStyle from "./components/GlobalStyle";
 //Import pages
 import AboutUs from "./pages/AboutUs.js";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
