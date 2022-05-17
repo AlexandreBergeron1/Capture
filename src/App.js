1cb60e0d962f5f31b064a84afbba7107e04b61fa
ALEXANDRE BERGERON <balex2001@outlook.com>
2022-05-17 14:57:39 -0400

    8. React Router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 //Styles
 import GlobalStyle from "./components/GlobalStyle";
 //Import pages
-import AboutUs from "./pages/AboutUs.js";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Import Components
 import Nav from "./components/Nav.js";
+//Router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/about" exact element={<AboutUs />} />
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
