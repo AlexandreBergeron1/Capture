69f0e97932076d6480c4260cacc1c35a18517700
ALEXANDRE BERGERON <balex2001@outlook.com>
2022-05-17 14:05:39 -0400

    7. Nav

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //Import pages
 import AboutUs from "./pages/AboutUs.js";
+import Nav from "./components/Nav.js";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
